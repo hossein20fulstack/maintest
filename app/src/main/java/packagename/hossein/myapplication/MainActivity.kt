493717d457a66808381d92c29e3cf2ae95cb8dec
hossein004 <hossein004@gmail.com>
2025-02-07 11:25:57 +0330

    first Commit

---
+++ app/src/main/java/packagename/hossein/myapplication/MainActivity.kt
@@ -0,0 +1,29 @@
+package packagename.hossein.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        CoroutineScope(Dispatchers.Main).launch{
+            Toast.makeText(this@MainActivity, "Hello", Toast.LENGTH_SHORT).show()
+            Log.i("TEST","test")
+        }
+    }
+}
